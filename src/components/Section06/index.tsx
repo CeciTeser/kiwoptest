4a462a0e07ee440ed426d8edc535eb61e58ca364
Austyn <austyn@MacBook-Air-de-Austyn.local>
2022-02-16 15:28:22 +0100

    set section 06

---
+++ src/components/Section06/index.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react"
+
+
+import './styles.scss';
+
+const Section06: FC = () => {
+
+    return (
+        
+        <section className="section06">   
+
+            <div className="text-box">
+                <h5>Where ideas become opportunities.</h5>
+                <p>Where engineers love to work.</p>
+            </div>
+            <div className="rectangle-101"></div>
+            <div className="rectangle-66"></div>
+
+            <button>TRABAJA CON NOSOTROS</button>
+          
+        </section>
+    
+      
+    )
+
+}
+
+export { Section06 }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,6 +6,7 @@ import { Section01 } from '../../components/Section01';
 import { Section02 } from '../../components/Section02';
 import { Section03 } from '../../components/Section03';
 import { Section04 } from '../../components/Section04';
+import { Section06 } from '../../components/Section06';
 
 import './styles.scss';
 
@@ -26,6 +27,8 @@ const Dashboard:FC =()=>{
             <Section03/>
 
             <Section04/>
+
+            <Section06/>
 
             <Footer/>
             
